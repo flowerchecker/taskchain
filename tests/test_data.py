476e367464e92721d4e73b911681dec50118f622
Thran <thran@centrum.cz>
2020-04-13 00:25:20 +0200

    add test of in memory data

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,21 +1,19 @@
-from taskchain.task import Task, Config
-
-
-class A(Task):
-
-    class Meta:
-        task_group = 'x'
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.run_called = 0
-
-    def run(self) -> int:
-        self.run_called += 1
-        return 1
+from taskchain.task import Task, Config, InMemoryData
 
 
 def test_persistence(tmp_path):
+    class A(Task):
+        class Meta:
+            task_group = 'x'
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.run_called = 0
+
+        def run(self) -> int:
+            self.run_called += 1
+            return 1
+
     config = Config(tmp_path, name='test')
 
     a = A(config)
@@ -35,3 +33,59 @@ def test_persistence(tmp_path):
     a3 = A(config2)
     assert a3.value == 1
     assert a3.run_called == 1
+
+
+def test_in_memory_data(tmp_path):
+    class A(Task):
+        class Meta:
+            task_group = 'x'
+            data_class = InMemoryData
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.run_called = 0
+
+        def run(self) -> int:
+            self.run_called += 1
+            return 1
+
+    config = Config(tmp_path, name='test')
+
+    a = A(config)
+    assert a.value == 1
+    assert a.run_called == 1
+    assert a.value == 1
+    assert a.run_called == 1
+
+    assert not (tmp_path / 'x' / 'a').exists()
+
+    a2 = A(config)
+    assert a2.value == 1
+    assert a2.run_called == 1
+
+
+def test_returned_in_memory_data(tmp_path):
+    class B(Task):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.run_called = 0
+
+        def run(self) -> InMemoryData:
+            self.run_called += 1
+            data = InMemoryData()
+            data._value = 1
+            return data
+
+    config = Config(tmp_path, name='test')
+
+    b = B(config)
+    assert b.value == 1
+    assert b.run_called == 1
+    assert b.value == 1
+    assert b.run_called == 1
+
+    assert not (tmp_path / 'x' / 'b').exists()
+
+    a2 = B(config)
+    assert a2.value == 1
+    assert a2.run_called == 1
