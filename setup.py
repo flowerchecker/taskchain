f38346a3c7c285ff82be5d7c65d9d231aa491b22
Jiří Thran Řihák <exthran@gmail.com>
2021-01-10 11:12:15 +0100

    update minimal python version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.8',
     install_requires=[
         'tqdm',
         'pandas',
