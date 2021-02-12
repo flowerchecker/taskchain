bd0ddea9810568b345dc178723dd2bab5bec5c41
Jiří Thran Řihák <exthran@gmail.com>
2021-02-12 08:27:32 +0100

    add parallel_map to utils

--- src/taskchain/utils/iter.py
+++ src/taskchain/utils/iter.py
@@ -1,3 +1,6 @@
+import asyncio
+import concurrent.futures
+
 from .jupyter import in_ipynb
 from tqdm import tqdm, tqdm_notebook
 
@@ -9,3 +12,24 @@ def progress_bar(data, use_tqdm=True, smoothing=0, **kwargs):
         iter_fce = tqdm_notebook if False else tqdm
         kwargs['smoothing'] = smoothing
     return iter_fce(data, **kwargs)
+
+
+def parallel_map(fun, iterable, threads=2, desc='Running tasks in parallel.', total=None):
+    if total is None and hasattr(iterable, '__len__'):
+        total = len(iterable)
+
+    def _fun(i, arg):
+        return i, fun(arg)
+
+    async def _run():
+        with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
+            loop = asyncio.get_event_loop()
+            futures = [
+                loop.run_in_executor(
+                    executor, _fun, i, input_value
+                ) for i, input_value in enumerate(iterable) ]
+            return [await output_value for output_value in progress_bar(asyncio.as_completed(futures), desc=desc, total=total)]
+
+    loop = asyncio.get_event_loop()
+    result = loop.run_until_complete(_run())
+    return [res for _, res in sorted(result, key=lambda ires: ires[0])]
