eafec76fb85d6fecf0afb376f5f5f1cda3c0a921
Jiří Thran Řihák <exthran@gmail.com>
2021-02-12 16:21:13 +0100

    improve progressbar in parallel_map

--- src/taskchain/utils/iter.py
+++ src/taskchain/utils/iter.py
@@ -5,7 +5,7 @@ from .jupyter import in_ipynb
 from tqdm import tqdm, tqdm_notebook
 
 
-def progress_bar(data, use_tqdm=True, smoothing=0, **kwargs):
+def progress_bar(data, use_tqdm=True, smoothing=0., **kwargs):
     def iter_fce(iterator, **kwargs):
         return iterator
     if use_tqdm:
@@ -14,7 +14,7 @@ def progress_bar(data, use_tqdm=True, smoothing=0, **kwargs):
     return iter_fce(data, **kwargs)
 
 
-def parallel_map(fun, iterable, threads=2, desc='Running tasks in parallel.', total=None):
+def parallel_map(fun, iterable, threads=2, desc='Running tasks in parallel.', total=None, smoothing=0.3):
     if total is None and hasattr(iterable, '__len__'):
         total = len(iterable)
 
@@ -28,7 +28,11 @@ def parallel_map(fun, iterable, threads=2, desc='Running tasks in parallel.', to
                 loop.run_in_executor(
                     executor, _fun, i, input_value
                 ) for i, input_value in enumerate(iterable) ]
-            return [await output_value for output_value in progress_bar(asyncio.as_completed(futures), desc=desc, total=total)]
+            return [
+                await output_value
+                for output_value
+                in progress_bar(asyncio.as_completed(futures), desc=desc, total=total, smoothing=smoothing)
+            ]
 
     loop = asyncio.get_event_loop()
     result = loop.run_until_complete(_run())
