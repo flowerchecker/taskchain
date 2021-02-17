79d076928c3e5cee3d919ec8b43ac46adf0099ea
Jiří Thran Řihák <exthran@gmail.com>
2021-02-17 12:02:13 +0100

    enable widget progress bar in notebooks

--- src/taskchain/utils/iter.py
+++ src/taskchain/utils/iter.py
@@ -9,7 +9,7 @@ def progress_bar(data, use_tqdm=True, smoothing=0., **kwargs):
     def iter_fce(iterator, **kwargs):
         return iterator
     if use_tqdm:
-        iter_fce = tqdm_notebook if False else tqdm
+        iter_fce = tqdm_notebook if in_ipynb() else tqdm
         kwargs['smoothing'] = smoothing
     return iter_fce(data, **kwargs)
 
