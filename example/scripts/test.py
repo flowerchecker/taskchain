fd953a662987b92d4574d01b123fd5011acb5553
Jiří Thran Řihák <exthran@gmail.com>
2021-06-29 17:34:38 +0200

    add directors to example projects

--- example/scripts/test.py
+++ example/scripts/test.py
@@ -5,7 +5,13 @@ from movie_ratings import config
 
 config_name = 'movies/20210624.imdb.yaml'
 chain = Config(config.TASKS_DIR, config.CONFIGS_DIR / config_name, global_vars=config).chain()
-
 print(chain)
 
-print(chain.all_movies.value)
+
+chain.force('movies')
+
+print(chain.movies.value.columns)
+# print(chain.directors.value)
+print(chain.directors.run_info['log'])
+print(chain.selected_directors.value)
+print(chain.selected_directors.run_info['log'])
--- example/src/movie_ratings/tasks/movies.py
+++ example/src/movie_ratings/tasks/movies.py
@@ -1,4 +1,6 @@
+from collections import defaultdict
 from pathlib import Path
+from typing import List, Dict, Generator
 
 import pandas as pd
 
@@ -6,7 +8,7 @@ from taskchain.task import ModuleTask
 from taskchain.task.parameter import Parameter
 
 
-class AllMovies(ModuleTask):
+class Movies(ModuleTask):
 
     class Meta:
         input_tasks = []
@@ -16,4 +18,45 @@ class AllMovies(ModuleTask):
 
     def run(self, source_file) -> pd.DataFrame:
         df = pd.read_csv(source_file)
+        df.set_index(df.imdb_title_id, inplace=True)
         return df
+
+
+class Directors(ModuleTask):
+    class Meta:
+        input_tasks = [Movies]
+        parameters = []
+
+    def run(self, movies) -> Dict:
+        directors = defaultdict(list)
+        for movie in movies.itertuples():
+            if pd.isna(movie.director):
+                continue
+            for director in movie.director.split(', '):
+                directors[director.strip()].append(movie.Index)
+
+        self.save_to_run_info(f'Directors count: {len(directors)}')
+        return dict(directors)
+
+
+class SelectedDirectors(ModuleTask):
+    class Meta:
+        input_tasks = [Directors, Movies]
+        parameters = [
+            Parameter('director_minimal_movie_count'),
+            Parameter('director_minimal_movie_rating', default=7),
+        ]
+
+    def run(self, directors, movies, director_minimal_movie_count, director_minimal_movie_rating) -> Generator:
+        selected_directors_count = 0
+        for director, directors_movies in directors.items():
+            good_movies = [m for m in directors_movies
+                           if self.get_movie_rating(movies, m) >= director_minimal_movie_rating]
+            if len(good_movies) >= director_minimal_movie_count:
+                selected_directors_count += 1
+                yield director
+
+        self.save_to_run_info(f'Selected directors count: {selected_directors_count}')
+
+    def get_movie_rating(self, movies, movie):
+        return movies.loc[movie].avg_vote
