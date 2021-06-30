63be7f5bab20423b810794894fbaade2ee9e5b59
Jiří Thran Řihák <exthran@gmail.com>
2021-06-30 10:22:35 +0200

    install icecream globally

--- src/taskchain/task/__init__.py
+++ src/taskchain/task/__init__.py
@@ -2,3 +2,5 @@ from .task import Task, ModuleTask
 from .data import Data, InMemoryData, JSONData, FileData
 from .config import Config, Context
 from .chain import Chain, MultiChain
+from icecream import install
+install()
