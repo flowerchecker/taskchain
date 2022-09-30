aeb22749bee60ac2a8512e05a5cf4738d64422f0
Jan Papousek <jan.papousek@flowerchecker.com>
2022-09-30 09:33:40 +0200

    feat: use chunks for multithreading parallelism

--- src/taskchain/utils/threading.py
+++ src/taskchain/utils/threading.py
@@ -1,8 +1,11 @@
 import asyncio
 import concurrent.futures
+import gc
 from typing import Callable, Iterable
 
-from .iter import progress_bar
+from tqdm.auto import tqdm
+
+from .iter import chunked
 
 
 def parallel_map(
@@ -13,6 +16,7 @@ def parallel_map(
     use_tqdm: bool = True,
     desc: str = 'Running tasks in parallel.',
     total: int = None,
+    chunksize: int = 1000,
 ):
     """
     Map function to iterable in multiple threads.
@@ -29,24 +33,37 @@ def parallel_map(
     Returns:
         list: of returned values by fce
     """
+    iterable = iterable
+    if isinstance(iterable, list) and total is None:
+        total = len(iterable)
+    if threads == 1:
+        return [fun(v) for v in tqdm(iterable, desc=desc, total=total, maxinterval=2) if use_tqdm else iterable]
 
     def _fun(i, arg):
         return i, fun(arg)
 
-    async def _run():
+    pbar = tqdm(desc=desc, total=total, maxinterval=2) if use_tqdm else None
+
+    async def _run(chunk):
         with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
             loop = asyncio.get_event_loop()
-            futures = [loop.run_in_executor(executor, _fun, i, input_value) for i, input_value in enumerate(iterable)]
-            return [
-                await output_value
-                for output_value in progress_bar(
-                    asyncio.as_completed(futures), use_tqdm=use_tqdm, desc=desc, total=total
-                )
-            ]
+            futures = [loop.run_in_executor(executor, _fun, i, input_value) for i, input_value in enumerate(chunk)]
+            result = []
+            for output_value in asyncio.as_completed(futures):
+                to_append = await output_value
+                if use_tqdm:
+                    pbar.update()
+                result.append(to_append)
+            return result
+        gc.collect()
 
     loop = asyncio.get_event_loop()
-    result = loop.run_until_complete(_run())
-    return [res for _, res in (sorted(result, key=lambda ires: ires[0]) if sort else result)]
+    result = []
+    for chunk in chunked(iterable, chunksize=chunksize):
+        chunk_result = loop.run_until_complete(_run(chunk))
+        for _, res in sorted(chunk_result, key=lambda ires: ires[0]) if sort else chunk_result:
+            result.append(res)
+    return result
 
 
 def parallel_starmap(fun: Callable, iterable: Iterable, **kwargs):
